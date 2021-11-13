a0b21e5d3a736c4d479681f1024f3c3485128951
MangoStore-sdk <alexlivemurithi@gmail.com>
2021-11-13 08:21:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# flask-domain-checker
+A flask app to search domain name info such as registration, expiry date and also the name of the registrar
+
+#@,/>
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask  
+from flask import request 
+from flask import render_template 
+import whois 
+app = Flask(__name__)
+
+@app.route("/", methods=['POST','GET'])
+def index():
+    if request.method == "POST":
+        domain = request.form['domain']
+        if domain == "":
+            return render_template("index.html")
+
+        try:
+            info = whois.whois(domain)
+            dat = [
+                    {"registrar":info.registrar},
+                    {"server":info.whois_server},
+                    {"date":info.creation_date},
+                    {"expire":info.expiration_date}
+            ]
+        except Exception as e:
+            print(e)
+
+        return render_template("index.html",data=dat)
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
